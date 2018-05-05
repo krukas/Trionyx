4461810095f1d99bffe94a72570c5692af5c6799
Krukas <maikel_martens_1@hotmail.com>
2018-05-05 16:00:34 +0200

    [TASK] Add jsonfy tag + content header block

--- trionyx/trionyx/templatetags/trionyx.py
+++ trionyx/trionyx/templatetags/trionyx.py
@@ -5,7 +5,11 @@ trionyx.trionyx.templatetags.trionyx
 :copyright: 2018 by Maikel Martens
 :license: GPLv3
 """
+import json
 from django import template
+from django.core.serializers import serialize
+from django.db.models.query import QuerySet
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -22,3 +26,10 @@ def active_menu_item(request, item):
 def render_component(context, component):
     """Render given component"""
     return component.render(context.flatten(), context.request)
+
+
+@register.filter
+def jsonify(obj):
+    if isinstance(obj, QuerySet):
+        return mark_safe(serialize(obj))
+    return mark_safe(json.dumps(obj))
