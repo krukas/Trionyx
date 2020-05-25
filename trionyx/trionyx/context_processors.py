d824906686b333984e61485ecbc3b85842d4e8c6
Krukas <maikel_martens_1@hotmail.com>
2020-05-25 20:50:24 +0100

    [TASK] Make favicon base64

--- trionyx/trionyx/context_processors.py
+++ trionyx/trionyx/context_processors.py
@@ -5,6 +5,11 @@ trionyx.trionyx.context_processors
 :copyright: 2018 by Maikel Martens
 :license: GPLv3
 """
+import os
+import base64
+
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 from django.conf import settings
 from django.apps import apps
 from trionyx.menu import app_menu
@@ -29,6 +34,41 @@ def offline_context():
     return [context]
 
 
+def generate_base64_favicon():
+    """Generate base64 favicon"""
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    skins = {
+        'blue': (0, 115, 183),
+        'yellow': (243, 156, 18),
+        'green': (0, 166, 90),
+        'purple': (96, 92, 168),
+        'red': (221, 75, 57),
+        'black': (17, 17, 17),
+    }
+    theme = settings.TX_THEME_COLOR.replace('-light', '')
+    color = skins.get(theme, (17, 17, 17))
+
+    text = '{}{}'.format(
+        settings.TX_LOGO_NAME_SMALL_START.upper(),
+        settings.TX_LOGO_NAME_SMALL_END.lower()
+    )
+
+    img = Image.new('RGB', (32, 32), color=color)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(os.path.join(app_dir, 'static', 'fonts', 'pt-mono.bold.ttf'), 24)
+
+    text_width, text_height = draw.textsize(text, font)
+    position = ((32 - text_width) / 2, (32 - 5 - text_height) / 2)
+    draw.text(position, text, (255, 255, 255), font=font)
+
+    buffered = BytesIO()
+    img.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode()
+
+
+tx_base64_icon = generate_base64_favicon()
+
+
 def trionyx(request):
     """Add trionyx context data"""
     locale, *_ = utils.get_current_locale().split('_')
@@ -40,6 +80,7 @@ def trionyx(request):
         'TX_LOGO_NAME_SMALL_START': settings.TX_LOGO_NAME_SMALL_START,
         'TX_LOGO_NAME_SMALL_END': settings.TX_LOGO_NAME_SMALL_END,
         'TX_THEME_COLOR': settings.TX_THEME_COLOR,
+        'tx_base64_icon': tx_base64_icon,
         'tx_tasks_url': model_url(get_class('trionyx.Task'), 'list'),
         'tx_version': __version__,
         'tx_show_changelog': (
